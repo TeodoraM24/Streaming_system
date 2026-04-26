27d2bf16852ae4ba08f0f584355ed42872545a28
MASIH1903 <144231041+MASIH1903@users.noreply.github.com>
2026-04-26 19:32:47 +0200

    jeg har renamed streaming_schema filen til streamingsystem-create-db

--- SQL/Scripts/test_business_logic.sql
+++ SQL/Scripts/test_business_logic.sql
@@ -1,7 +1,7 @@
 -- =========================================================
 -- TEST SCRIPT FOR STREAMING BUSINESS LOGIC
 -- Run after:
--- 1. streaming_schema.sql
+-- 1. streamingsystem-create-db.sql
 -- 2. streaming-system-v2-postgres-seed-fixed.sql
 -- 3. streaming_business_logic.sql
 -- =========================================================
