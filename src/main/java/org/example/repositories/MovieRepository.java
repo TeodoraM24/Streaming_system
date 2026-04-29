04242db7eec9d99dcaa485bd5f40e0ae0b41e052
MASIH1903 <144231041+MASIH1903@users.noreply.github.com>
2026-04-29 12:05:24 +0200

    tilføjet hele idea mappen til gitignore så vi undgår at pushe xml filer op til repo

--- src/main/java/org/example/repositories/MovieRepository.java
+++ src/main/java/org/example/repositories/MovieRepository.java
@@ -3,4 +3,13 @@ import org.example.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {}
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+
+
+    
+
+
+}
+
+
