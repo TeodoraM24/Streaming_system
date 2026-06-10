afe62ca743d14770f3dc34b9740328dfa08487fe
Umair-230104 <cph-ut38@cphbusiness.dk>
2026-06-10 21:48:48 +0200

    test

--- src/main/java/org/example/repositories/MovieRepository.java
+++ src/main/java/org/example/repositories/MovieRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
-    List<Account> findTop10ByOrderByContent_RatingDesc();
+    List<Movie> findTop10ByOrderByContent_RatingDesc();
     List<Movie> findByContent_Genres_GenreId(Long genreId);
 }
 
